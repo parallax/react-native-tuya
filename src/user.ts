d2879bb19a97e98bf981810f826174ee1c9f7cc5
Dan Pegg <daniel.pegg7849@gmail.com>
2023-09-01 11:01:36 +0100

    Update user.ts

--- src/user.ts
+++ src/user.ts
@@ -39,9 +39,7 @@ export function logout(): Promise<string> {
 }
 
 export async function getCurrentUser(): Promise<User | null> {
-  const user = await tuya.getCurrentUser();
-  // The iOS SDK returns an empty user model but the Android one doesn't.
-  return user && user.email ? user : null;
+  return await tuya.getCurrentUser();
 }
 
 export function cancelAccount(): Promise<string> {
